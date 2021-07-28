5b72262048f427782e1aaa9cc9af430e8700ad62
adaltospjr <adaltospjr@icloud.com>
2021-07-27 21:18:18 -0300

    Atualizando listas de games

--- Flask parte 1/jogoteca.py
+++ Flask parte 1/jogoteca.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/inicio')
 def ola():
-    lista = ['Gears of War', 'Days Gone', 'AC Origins', 'Journey']
+    lista = ['Gears of War', 'Days Gone', 'AC Origins', 'Journey', 'Metal Gear']
     return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 #app.run(host='0.0.0.0', port=8080)
